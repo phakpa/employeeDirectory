5046052ad3f9df82e75979a49cc8b42025fc7e1f
Pha Kpa <kphaoctober@yahoo.com>
2020-09-26 19:27:09 -0400

    Updates

---
+++ precache-manifest.8e0adaf5bcc984c63f07f089089b9ccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0e5d54a82b2c4b20bad91e77105ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3465491b2cc10818fa1c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5b6a8325c2fa1a07c877",
+    "url": "/static/css/main.2cab1584.chunk.css"
+  },
+  {
+    "revision": "3465491b2cc10818fa1c",
+    "url": "/static/js/2.01edd394.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01edd394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6a8325c2fa1a07c877",
+    "url": "/static/js/main.f97df764.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  }
+]);
