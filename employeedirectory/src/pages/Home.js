ea36de9fcb359da97179ccdf49a58203ca0049da
Pha Kpa <kphaoctober@yahoo.com>
2020-09-26 05:36:38 -0400

    search by fullname

--- employeedirectory/src/pages/Home.js
+++ employeedirectory/src/pages/Home.js
@@ -41,6 +41,7 @@ class Home extends Component {
           phone.push(results[i].phone);
           email.push(results[i].email);
         }
+
         this.setState({
           result: results,
           image: image,
@@ -49,26 +50,106 @@ class Home extends Component {
           email: email,
           dob: newDob,
         });
-
-        console.log(this.state.image);
-        console.log(this.state.name);
-        console.log(this.state.phone);
-        console.log(this.state.email);
-        console.log(this.state.dob);
-        console.log(this.state.result);
       })
       .catch((err) => console.log(err));
   }
 
   handleInputChange = (event) => {
-    this.setState({ search: event.target.value });
-    console.log(event.target.value);
+    event.preventDefault();
+    let tempStr = event.target.value;
+    this.setState({
+      search: tempStr,
+    });
+
+    if (tempStr !== "") {
+      console.log("Search: " + tempStr);
+      //if search value true code here
+      let origResults = this.state.result;
+
+      let tempResult = origResults.filter((e) => {
+        return e.name.first === tempStr;
+      });
+      console.log(tempResult);
+
+      //   console.log(this.state.result);
+
+      let results = tempResult;
+      let image = [];
+      let name = [];
+      let phone = [];
+      let email = [];
+      let dob = results.map((e) => {
+        return e.dob.date.slice(0, 10);
+      });
+
+      let newDob = [];
+      for (let i = 0; i < dob.length; i++) {
+        let year = dob[i].slice(0, 4);
+
+        let month = dob[i].slice(5, 7);
+        let day = dob[i].slice(8, 10);
+        newDob.push(month + "-" + day + "-" + year);
+      }
+
+      for (let i = 0; i < results.length; i++) {
+        image.push(results[i].picture.medium);
+        name.push(results[i].name.first + " " + results[i].name.last);
+        phone.push(results[i].phone);
+        email.push(results[i].email);
+      }
+
+      this.setState({
+        image: image,
+        name: name,
+        phone: phone,
+        email: email,
+        dob: newDob,
+      });
+    } else {
+      console.log("Search: " + tempStr);
+      //if nothing in search code here
+      let results = this.state.result;
+      let image = [];
+      let name = [];
+      let phone = [];
+      let email = [];
+      let dob = results.map((e) => {
+        return e.dob.date.slice(0, 10);
+      });
+
+      let newDob = [];
+      for (let i = 0; i < dob.length; i++) {
+        let year = dob[i].slice(0, 4);
+
+        let month = dob[i].slice(5, 7);
+        let day = dob[i].slice(8, 10);
+        newDob.push(month + "-" + day + "-" + year);
+      }
+
+      for (let i = 0; i < results.length; i++) {
+        image.push(results[i].picture.medium);
+        name.push(results[i].name.first + " " + results[i].name.last);
+        phone.push(results[i].phone);
+        email.push(results[i].email);
+      }
+
+      this.setState({
+        image: image,
+        name: name,
+        phone: phone,
+        email: email,
+        dob: newDob,
+      });
+    }
   };
 
   render() {
     return (
       <div>
-        <SearchForm handleInputChange={this.handleInputChange} />
+        <SearchForm
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
         <TableEmployee
           image={this.state.image}
           name={this.state.name}
